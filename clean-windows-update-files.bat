edda33d5cfdfa4287e4a6fedf6a13460c801f469
Yago Saraiva <yago.aparecido@hotmail.com>
2023-12-19 10:37:19 -0300

    Create clean-windows-update-files.bat

---
+++ clean-windows-update-files.bat
@@ -0,0 +1,28 @@
+@echo    *****************************************
+@echo    ***  DELETANDO E ALTERANDO DIRETORIO  ***
+@echo    ***   DE ARQUIVOS DO WINDOWS UPDATE   ***
+@echo    *****************************************
+
+@echo PARANDO O SERVICO WINDOWS UPDATE
+net stop wuauserv
+
+@echo PARANDO O SERVICO BITS
+net stop bits
+
+@echo DELETANDO O DIRETORIO DE ARQUIVOS DO WINDOWS UPDATE
+if exist C:\Windows\SoftwareDistribution rmdir /S /Q C:\Windows\SoftwareDistribution
+
+@echo CRIANDO O NOVO DIRETORIO DE ARQUIVOS DO WINDOWS UPDATE
+if not exist D:\SoftwareDistribution mkdir D:\NovaPastaAtualizacoes
+
+@echo CRIANDO UM LINK SIMBOLICO PARA O NOVO DIRETORIO DE ARQUIVOS DO WINDOWS UPDATE
+CD /D C:\Windows
+mklink /J SoftwareDistribution D:\NovaPastaAtualizacoes
+
+@echo INICIANDO O SERVICO WINDOWS UPDATE
+net start wuauserv
+
+@echo INICIANDO O SERVICO BITS
+net start bits
+
+pause
